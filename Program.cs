b52a061f22d2cd2eb5ec9fe399a51eef45babf56
neffarrty <109294033+neffarrty@users.noreply.github.com>
2022-07-14 17:19:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeNameColor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            var age = Console.ReadLine();
+            Console.Write("Enter your favourite color: ");
+            var favColor = Console.ReadLine();
+
+            Console.WriteLine($"Well, you'll be surprised, but your name is {name}, you're {age} years old and you prefer wearing {favColor} T-shirt");
+            
+            Console.ReadKey();
+        }
+    }
+}
